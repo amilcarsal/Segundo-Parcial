b3cce03da09e98e0e736158234daab08f150a5d0
amilcarsal <135087213+amilcarsal@users.noreply.github.com>
2024-09-28 12:16:55 -0600

    Update
    corrección de codigo

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Importa desde 'react-dom/client' para React 18
-import './index.css'; // Asegúrate de importar tus estilos (si tienes)
-import App from './App'; // Importa el componente principal App
+import ReactDOM from 'react-dom/client'; 
+import './index.css'; 
+import App from './App'; 
 
-// Renderiza la aplicación en el elemento con id 'root'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
